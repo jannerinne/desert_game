bcefcf33a064373991fc932ab4e9a39bf9699b55
maqqr <maqqr@users.noreply.github.com>
2015-03-27 01:20:58 +0200

    pelaajalle laitettu animaatiot ja korjattu graafisia vikoja

---
+++ Assets/Scripts/EventStartPlace.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventStartPlace : MonoBehaviour {
+
+	public Vector2 StartOffset;
+
+	// Use this for initialization
+	void Start () {
+		float x = transform.position.x + StartOffset.x;
+		float y = transform.position.y + StartOffset.y;
+		transform.position = new Vector3(x, y, transform.position.z);
+	}
+}
--- Assets/Scripts/GameManager.js
+++ Assets/Scripts/GameManager.js
@@ -4,6 +4,7 @@ import System.Collections.Generic;
 var player : GameObject;
 var ground : GameObject;
 var background : GameObject;
+var background2 : GameObject;
 
 // Lista mahdollisista tapahtumista. Tapahtumat pitää olla prefabbeina.
 var eventGroup1 : List.<Transform> = List.<Transform>();
@@ -17,12 +18,13 @@ var events3 : int = 0;
 
 var eventGroupNumber : int = 1; // Minkä ryhmän eventtejä otetaan.
 
-var walkSpeed : float = 0.5;               // Pelaajan kävelynopeus.
-var relativeBackgroundSpeed : float = 0.5; // Taustan suhteellinen nopeus pelaajaan.
-var eventTimer : float = 10.0;             // Montako sekuntia pitää kävellä ennen kuin tapahtuma tulee.
+var walkSpeed : float = 0.5;                // Pelaajan kävelynopeus.
+var relativeBackgroundSpeed : float = 0.3;  // Taustan suhteellinen nopeus pelaajaan.
+var relativeBackground2Speed : float = 0.1; // Taaemman taustan suhteellinen nopeus pelaajaan.
+var eventTimer : float = 10.0;              // Montako sekuntia pitää kävellä ennen kuin tapahtuma tulee.
 
 private var scroll : float; // Paljonko ollaan liikuttu.
-
+private var playerDir : float = 1; // Pelaajan suunta, joko -1 tai 1.
 
 function Start () {
 }
@@ -32,18 +34,31 @@ function Update () {
 	// ------- Liikkuminen -------
 	var direction = Input.GetAxis("Horizontal");
 	
+	// Pelaajan animaation parametrit.
+	var anim = player.GetComponent(typeof(Animator));
+	anim.SetFloat("PlayerSpeed", Mathf.Abs(direction));
+	
+	// Spriten pyöritys skaalauksella liikkumissuunnan mukaan.
+	if (direction > 0.0) playerDir = 1;
+	if (direction < 0.0) playerDir = -1;
+	var spriteScale = player.transform.localScale;
+	spriteScale.x = Mathf.Abs(spriteScale.x) * playerDir;
+	player.transform.localScale = spriteScale;
+	
+	ground.renderer.material.SetTextureOffset("_MainTex", Vector2(scroll, -0.005f));
+	background.renderer.material.SetTextureOffset("_MainTex", Vector2(scroll * relativeBackgroundSpeed, -0.005f));
+	background2.renderer.material.SetTextureOffset("_MainTex", Vector2(scroll * relativeBackground2Speed, 0));
+	
 	if (direction != 0.0) {
 		var movement = walkSpeed * Time.smoothDeltaTime * direction;
 		eventTimer -= Time.smoothDeltaTime;
 		
 		// Liikuta maata ja taustaa.
 		scroll += movement;
-		ground.renderer.material.SetTextureOffset("_MainTex", Vector2(scroll, 0));
-		background.renderer.material.SetTextureOffset("_MainTex", Vector2(scroll * relativeBackgroundSpeed, 0));
 		
 		// Liikuta objekteja.
 		for (var obj : GameObject in GameObject.FindGameObjectsWithTag("EventObject")) {
-			obj.transform.position.x -= movement * ground.renderer.bounds.size.x * 0.5;
+			obj.transform.position.x -= movement * ground.renderer.bounds.size.x;
 			
 			// Jos objekti on liian kaukana, niin se poistetaan.
 			if (Mathf.Abs(obj.transform.position.x) > ground.renderer.bounds.size.x * 0.5 + 0.3) {
@@ -57,7 +72,9 @@ function Update () {
 	if (direction != 0.0) {
 		if (eventTimer < 0.0) {
 			// Seuraava tapahtuma tulee 20 - 50 sek. kävelyn kuluttua.
-			eventTimer = 20 + Random.value * 30;
+			//eventTimer = 20 + Random.value * 30;
+			
+			eventTimer = 5; // testin vuoksi 5 sek.
 			
 			var event = RandomEvent();
 			if (event != null) {
